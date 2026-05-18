567f1381d238f0788438649a2464ca00c54665b3
SamW-1 <saw231@exeter.ac.uk>
2026-05-18 16:56:42 +0100

    Implemented brute-force solution for TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -2,4 +2,13 @@ class TwoSum {
     public static void main (String args[]) {
         System.out.println("Testing");
     }
+    public int[] twoSum(int[] nums, int target) {
+        // Brute Force Solution: Take each number and add it to every other number in nums
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) { return new int[] {i, j}; }
+            }
+        }
+        return null;
+    }
 }
